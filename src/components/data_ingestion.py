efd961342b80ba6935a5d59ad5c64950322d6d01
Sanat-007 <sanat18102007@gmail.com>
2026-05-16 18:58:28 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -37,7 +37,7 @@ class DataIngestion:
             logging.info("Ingestion of the data is completed.")
 
             return(
-                self.ingestion_config.test_data_path,
+                self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
             )
 
